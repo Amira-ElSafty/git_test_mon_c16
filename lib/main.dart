0b8704275e275844aa9cc6f8c6e258be6da7d109
DESKTOP-NR6TVAI\LAPTOP <amiraelsaftytraining@gmail.com>
2025-08-25 21:00:25 +0300

    + added appBar in Scaffold.

--- lib/main.dart
+++ lib/main.dart
@@ -31,7 +31,11 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: Scaffold(),
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.orange,
+        ),
+      ),
     );
   }
 }
